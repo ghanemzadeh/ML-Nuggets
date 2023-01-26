4dc566efaf7801f4634d236a69eb2a57a990caa2
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2023-01-26 14:01:22 +0300

    Some minor update

---
+++ HousePrice/HousePricePrediction.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import pylab as pl
+import numpy as np
+
+df = pd.read_csv("housePrice.csv")
+
+# take a look at the dataset
+print(df.head())
+#print(df.tail())
+print(df.shape)
+#print(df.dtypes)
+#Data cleansing for Area and Address features
+df = df[pd.to_numeric(df["Area"], errors="coerce").notnull()]
+df["Area"] = df["Area"].astype("int")
+print(df.shape)
+df=df.dropna()
+df["Address"] = df["Address"].astype("string")
+print(df.shape)
+print(df.dtypes)
+
